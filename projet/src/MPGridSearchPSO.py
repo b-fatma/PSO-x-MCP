70fb1f7d5207cdba344b17bf49c84690fbbfe518
b-fatma <fz.boulgheraof@gmail.com>
2025-04-04 08:11:23 +0100

    tests + results

--- projet/src/MPGridSearchPSO.py
+++ projet/src/MPGridSearchPSO.py
@@ -78,7 +78,7 @@ class MPGridSearchPSO:
 
 
 
-
+# Already run
 def run_GS_BPSO_HDBPSO():
     dataset_folder = "../data"
     test_files = sorted([filename for filename in os.listdir(dataset_folder)])[::-1] 
@@ -107,7 +107,7 @@ def run_GS_BPSO_HDBPSO():
 
 
 
-
+# Already run
 def run_GS_wHDBPSO():
     dataset_folder = "../data"
     test_files = sorted([filename for filename in os.listdir(dataset_folder)])[::-1] 
@@ -164,6 +164,8 @@ def run_GS_mutation_BPSO():
     grid_search.save_results_to_csv("../stats/mutation_bpso.csv")
     print(f"Run took {time.time()-start_time:.2}s\n\n")
 
+
+
 def run_GS_stochastic_mutation_BPSO():
     dataset_folder = "../data"
     test_files = sorted([filename for filename in os.listdir(dataset_folder)])[::-1] 
@@ -193,9 +195,12 @@ def run_GS_stochastic_mutation_BPSO():
     print(f"Run took {time.time()-start_time:.2}s\n\n")
 
 
-def run_GS_stochastic_BPSO_HDBPSO():
+
+
+def run_GS_stochastic_BPSO_HDBPSO_ryma():
     dataset_folder = "../data"
-    test_files = sorted([filename for filename in os.listdir(dataset_folder)])[::-1] 
+    # test_files = sorted([filename for filename in os.listdir(dataset_folder)])[::-1] 
+    test_files = ['scpc5.txt', 'scpc4.txt', 'scpc3.txt', 'scpc2.txt', 'scpc1.txt', 'scp45.txt', 'scp44.txt', 'scp43.txt', 'scp42.txt', 'scp41.txt']
     print(test_files)
 
     param_grid = {
@@ -216,7 +221,35 @@ def run_GS_stochastic_BPSO_HDBPSO():
     start_time = time.time()
     grid_search = MPGridSearchPSO(dataset_folder, test_files, param_grid)
     grid_search.run_grid_search()
-    grid_search.save_results_to_csv("../stats/stochastic_bpso_hdbpso.csv")
+    grid_search.save_results_to_csv("../stats/stochastic_bpso_hdbpsoa_c_4[1,5].csv")
+    print(f"Run took {time.time()-start_time:.2}s\n\n")
+
+
+def run_GS_stochastic_BPSO_HDBPSO_fatma():
+    dataset_folder = "../data"
+    # test_files = sorted([filename for filename in os.listdir(dataset_folder)])[::-1] 
+    test_files = ['scpb5.txt', 'scpb4.txt', 'scpb3.txt', 'scpb2.txt', 'scpb1.txt', 'scpa5.txt', 'scpa4.txt', 'scpa3.txt', 'scpa2.txt', 'scpa1.txt', 'scp49.txt', 'scp48.txt', 'scp47.txt', 'scp46.txt', 'scp410.txt']
+    print(test_files)
+
+    param_grid = {
+        # Swarm parameters
+        'num_particles': [10, 30, 100],
+        'neighborhood_size': [None, 10, 30],
+        # Particle parameters
+        'inertia_type': ['linear', 'fixed'],
+        'inertia_value': [0.2, 0.5, 0.7], # Only used if inertia_type is 'fixed'
+        'c1': [1.5, 2.0],
+        'c2': [1.5, 2.0],
+        # Particle type parameters
+        'dist_type': ['bit-wise', 'HD'],
+        'selection_type': ['stochastic']
+    }
+
+    print(f"\nRunning Grid Search with optimize params: {param_grid}")
+    start_time = time.time()
+    grid_search = MPGridSearchPSO(dataset_folder, test_files, param_grid)
+    grid_search.run_grid_search()
+    grid_search.save_results_to_csv("../stats/stochastic_bpso_hdbpso_a_b_4[6,10].csv")
     print(f"Run took {time.time()-start_time:.2}s\n\n")
 
 
@@ -242,5 +275,5 @@ def main():
     print(f"Run took {time.time()-start_time:.2}s\n\n")
 
 if __name__ == "__main__":
-    run_GS_BPSO_HDBPSO()
+    run_GS_stochastic_BPSO_HDBPSO_ryma()
 
